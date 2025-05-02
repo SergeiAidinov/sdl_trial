73cecb51d723ffd448a6112bc9a778967eff5572
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-05-03 02:20:43 +0300

    mouse worked

--- main.c
+++ main.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
 //#include </usr/include/SDL2/SDL.h>
-#include <SDL2/SDL.h>
-#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 
 // Compiler options: -lSDL2 -lSDL2main
 // Command line:  gcc main.c -lSDL2 -lSDL2main -o main
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+int main(int argc, char *argv[]) {
+    printf("Hello World!\n");
+    //SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Рисование линии", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Белый цвет фона
+    SDL_Window *window = SDL_CreateWindow("Hello SDL3", 300, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, "qq");
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
     SDL_RenderClear(renderer);
+    SDL_Event event;
+    while (1) {
+        SDL_WaitEvent(&event);
+        if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+            if (event.button.button == SDL_BUTTON_LEFT) {
+                printf("Левая кнопка мыши нажата\n");
+            }
+        }
+        if (event.type == SDL_EVENT_MOUSE_MOTION)
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет линии
-    SDL_RenderDrawLine(renderer, 100, 100, 700, 500);
+                printf("Mouse moved\n");
 
-    SDL_RenderPresent(renderer);
+        }
+       // if (event.type != SDL_EVENT_FIRST) printf("Event: %d\n", event.type);
 
-    SDL_Delay(5000); // Задержка на 5 секунд
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    //SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1); // Красный цвет линии
+    //SDL_RenderLine(renderer, 100, 100, 300, 200);
+    //SDL_RenderClear(renderer);
+
+    //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1); // Белый цвет фона
+    //SDL_RenderClear(renderer);
+
+    //SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет линии
+    //SDL_RenderDrawLine(renderer, 100, 100, 700, 500);
+
+    //SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    //SDL_DestroyRenderer(renderer);
+    //SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
