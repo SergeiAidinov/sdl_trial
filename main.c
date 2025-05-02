e6bcc7aa66a6f0e8e4e1b67fbc5396f75f06ebd4
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-05-02 21:01:16 +0300

    draws line

--- main.c
+++ main.c
@@ -8,24 +8,22 @@
 // Command line:  gcc main.c -lSDL2 -lSDL2main -o main
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Window* window = SDL_CreateWindow("Рисование линии", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Белый цвет фона
+    SDL_RenderClear(renderer);
 
-    SDL_Delay(2000);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет линии
+    SDL_RenderDrawLine(renderer, 100, 100, 700, 500);
 
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000); // Задержка на 5 секунд
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
