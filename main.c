09e9df18b6174b2f4b345bf9e57c28801cde2699
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-05-31 12:13:46 +0300

    places crosses correctly

--- main.c
+++ main.c
@@ -3,25 +3,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include<unistd.h>
-#define _TTT_GAME_WINDOW_SIZE 600
 #define SQUARES_IN_LINE 3
+#define SQUARE_SIZE 200
+#define TTT_GAME_WINDOW_SIZE (SQUARE_SIZE * SQUARES_IN_LINE)
 struct color_rgb {
     int red, green, blue;
 };
+struct input {
+    int row_input;
+    int column_input;
+};
 struct color_rgb background_color = {240, 220, 130};
-const int SQUARE_SIZE = _TTT_GAME_WINDOW_SIZE / SQUARES_IN_LINE;
 
 // Compiler options: -lSDL3
 
 SDL_FRect createFRect(int x, int y, int width, int height);
 
+struct input defineSquare(float x_coord, float y_coord);
+
 int main(int argc, char *argv[]) {
     printf("Hello World!\n");
-    SDL_Window *window = SDL_CreateWindow("Hello SDL3", _TTT_GAME_WINDOW_SIZE, _TTT_GAME_WINDOW_SIZE, 0);
+    SDL_Window *window = SDL_CreateWindow("Hello SDL3", TTT_GAME_WINDOW_SIZE, TTT_GAME_WINDOW_SIZE, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
     SDL_SetRenderDrawColor(renderer, background_color.red, background_color.green, background_color.blue, 0);
     SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
     SDL_Surface *car = SDL_LoadBMP("/home/sergei/CLionProjects/sdl_trial/resources/cross.bmp");
     SDL_Surface *plane = SDL_LoadBMP("/home/sergei/CLionProjects/sdl_trial/resources/nought.bmp");
     SDL_Texture *car_texture = SDL_CreateTextureFromSurface(renderer, car);
@@ -31,9 +36,9 @@ int main(int argc, char *argv[]) {
     SDL_RenderTexture(renderer, car_texture, NULL, &car_frect);
     SDL_RenderTexture(renderer, plane_texture, NULL, &plane_frect);
     SDL_SetRenderDrawColor(renderer, 255, 0, 0,SDL_ALPHA_TRANSPARENT); // Красный цвет линии
-    for (float i = SQUARE_SIZE; i < _TTT_GAME_WINDOW_SIZE; i+= SQUARE_SIZE) {
-        SDL_RenderLine(renderer, i, 0, i, _TTT_GAME_WINDOW_SIZE);
-        SDL_RenderLine(renderer, 0, i, _TTT_GAME_WINDOW_SIZE, i);
+    for (float i = SQUARE_SIZE; i < TTT_GAME_WINDOW_SIZE; i+= SQUARE_SIZE) {
+        SDL_RenderLine(renderer, i, 0, i, TTT_GAME_WINDOW_SIZE);
+        SDL_RenderLine(renderer, 0, i, TTT_GAME_WINDOW_SIZE, i);
     }
     SDL_RenderPresent(renderer);
     SDL_UpdateWindowSurface(window);
@@ -44,10 +49,20 @@ int main(int argc, char *argv[]) {
             if (event.button.button == SDL_BUTTON_LEFT) {
                 float x_coord = event.button.x;
                 float y_coord = event.button.y;
+                struct input current_input = defineSquare(x_coord, y_coord);
+                printf("X: %i, Y: %i\n", current_input.row_input, current_input.column_input);
                 printf("%s %f %f\n", "Левая кнопка мыши нажата:", x_coord, y_coord);
+                SDL_FRect car_frect = createFRect(current_input.column_input * SQUARE_SIZE, current_input.row_input * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+                SDL_RenderTexture(renderer, car_texture, NULL, &car_frect);
+                for (float i = SQUARE_SIZE; i < TTT_GAME_WINDOW_SIZE; i+= SQUARE_SIZE) {
+                    SDL_RenderLine(renderer, i, 0, i, TTT_GAME_WINDOW_SIZE);
+                    SDL_RenderLine(renderer, 0, i, TTT_GAME_WINDOW_SIZE, i);
+                }
+                SDL_RenderPresent(renderer);
+                SDL_UpdateWindowSurface(window);
+
             }
         }
-        //if (event.type == SDL_EVENT_MOUSE_MOTION) printf("Mouse moved\n");
     }
 }
 
@@ -58,4 +73,11 @@ SDL_FRect createFRect(int x, int y, int width, int height) {
     rect.w = width;
     rect.h = height;
     return rect;
+}
+
+struct input defineSquare(float x_coord, float y_coord) {
+    int x = x_coord / SQUARE_SIZE;
+    int y = y_coord / SQUARE_SIZE;
+    struct input current_input = {y, x};
+    return current_input;
 }
